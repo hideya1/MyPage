1021395fbd847364e023903a69c428f9e8c6193e
hideya1 <watanabehideya@gmail.com>
2023-08-06 13:57:58 +0900

    update papers

---
+++ js/my_page.js
@@ -0,0 +1,17 @@
+const url = location.href,
+art = document.getElementsByClassName("art")[0],
+div = document.createElement("div"),
+en = document.createElement("a"),
+ja = document.createElement("a");
+ja.textContent = "日本語";
+ja.href = url.replace(".html", "_ja.html")
+ja.style.opacity = 0.6
+ja.style.textDecoration = "none";
+en.textContent = "英語"
+en.style.marginLeft = "1rem";
+en.style.textDecoration = "none";
+div.appendChild(ja);
+div.appendChild(en);
+div.style.alignSelf = "flex-end";
+
+art.insertBefore(div, art.firstChild)
---
+++ js/my_page_ja.js
@@ -0,0 +1,17 @@
+const url = location.href,
+art = document.getElementsByClassName("art")[0],
+div = document.createElement("div"),
+en = document.createElement("a"),
+ja = document.createElement("a");
+en.textContent = "English";
+en.href = url.replace("_ja.html", ".html")
+en.style.opacity = 0.6
+en.style.textDecoration = "none";
+ja.textContent = "Japanese"
+ja.style.marginLeft = "1rem";
+ja.style.textDecoration = "none";
+div.appendChild(en);
+div.appendChild(ja);
+div.style.alignSelf = "flex-end";
+
+art.insertBefore(div, art.firstChild)
